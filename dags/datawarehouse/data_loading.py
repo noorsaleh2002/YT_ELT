cc2c46bb718b39e44ba80fb64c9b828addaae576
noorsaleh2002 <115577009+noorsaleh2002@users.noreply.github.com>
2025-10-30 16:15:29 +0300

    getting the data from the json into the actual database tables

---
+++ dags/datawarehouse/data_loading.py
@@ -0,0 +1,20 @@
+#funtion for open the file json , read the file and parse it inot python object 
+import json
+from datetime import date
+import logging
+
+logger = logging.getLogger(__name__)
+
+def load_path():
+    file_path = f"./data/YI_data_{date.today()}.json"
+    try:
+        logger.info(f"Processing file: YI_data_{date.today()}")
+        with open(file_path, "r", encoding="utf-8") as raw_data:
+            data = json.load(raw_data)
+        return data
+    except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
+        raise
+    except json.JSONDecodeError:
+        logger.error(f"Invalid JSON in file: {file_path}")
+        raise
--- dags/datawarehouse/data_utils.py
+++ dags/datawarehouse/data_utils.py
@@ -1,3 +1,5 @@
+table ="yt_api"
+
 #create the helper funciton
 #here we will have the most funcitons relating to database connections and operations like creating tables
 #and schemas
@@ -21,3 +23,71 @@ def get_conn_cursor():
 def close_conn_cursor(conn,cur):
     cur.close() # to releas resourses
     conn.close()
+
+
+#build the function to create the schema 
+def create_schema(schema):
+    conn,cur=get_conn_cursor()
+
+    #create a schema so we can define the following SQL
+
+    schema_sql=f"CREATE SCHEMA IF NOT EXISTS {schema};"
+    cur.execute(schema_sql)
+    conn.commit()#since we are making changes to the database we need to commit these changes.
+    close_conn_cursor(conn,cur)
+#other function for the tables
+def create_table(schema):
+    conn,cur=get_conn_cursor()
+
+    if schema=='staging':
+        table_sql=f"""
+                    CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" VARCHAR(20) NOT NULL,
+                    "Video_Views" INT,
+                    "Likes_Count" INT,
+                    "Comments_Count" INT
+                    );
+            """
+    else:
+            table_sql=f"""
+                   CREATE TABLE IF NOT EXISTS {schema}.{table} (
+                    "Video_ID" VARCHAR(11) PRIMARY KEY NOT NULL,
+                    "Video_Title" TEXT NOT NULL,
+                    "Upload_Date" TIMESTAMP NOT NULL,
+                    "Duration" TIME NOT NULL,
+                    "Video_Type" VARCHAR(10) NOT NULL,
+                    "Video_Views" INT,
+                    "Likes_Count" INT,
+                    "Comments_Count" INT
+                );
+            """
+
+
+    cur.execute(table_sql)
+    conn.commit()
+    close_conn_cursor(conn,cur)
+
+
+
+#create a funtion that we should create is to get all the video IDs in either the staging or the correlation
+#this will be helpful when we come to loop through the rows of data indide the tables
+
+def get_video_ids(cur,schema):
+     cur.execute(f"SELECT Video_ID FROM {schema}.{table};")
+     ids=cur.fetchall() #this will give a list of dictinaries where the key is always this variable then the value.
+     #Ex: """ 
+     #[{'Video_ID':'anksdk'},{'Video_ID':'kjahkfjd'} ...]
+     # """
+
+
+     video_ids=[row["Video_ID"] for row in ids]  
+     return video_ids   
+    #Ex: """ 
+     #['anksdk''kjahkfjd' ...]
+     # """
+
+
+
